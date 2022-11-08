a19c70a453be78d14eaa3036e27aa079cc813edf
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-11-08 21:15:14 +0300

    Configure redux toolkit and fix linter errors

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Categories = () => (
-  <button type="button">Check Status</button>
-);
+const Categories = () => <button type="button">Check Status</button>;
 
 export default Categories;
